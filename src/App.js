82e21349213d470315be8132f5f1c1cfe13f2c6d
mtmwouter <74245592+mtmwouter@users.noreply.github.com>
2023-10-24 16:20:00 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App"> Weather app</div>
+    <div className="App"> 
+      <h1> Weather App</h1>
+      <footer> 
+        This project was coded by Micky Wouter and is {" "} 
+      <a href='https://github.com/mtmwouter/react-weather-app' 
+      target="_blank">
+      open-sourced on GitHub
+      </a>
+
+      </footer>
+    </div>
   );
 }
 
